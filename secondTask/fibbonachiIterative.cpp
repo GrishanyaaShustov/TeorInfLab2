146432f2699176f30b2e7a8d16def7904436b6a9
GrishanyaaShustov <dinsuil275283@gmail.com>
2024-12-15 15:03:18 +0500

    Create fibbonachiIterative.cpp

---
+++ secondTask/fibbonachiIterative.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    int prev1 = 0, prev2 = 1, current = 0;
+
+    for (int i = 2; i <= n; ++i) {
+        current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+
+    return current;
+}
+
+int main() {
+    int n;
+    std::cout << "Введите номер числа Фибоначчи: ";
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "Ошибка: номер числа Фибоначчи должен быть неотрицательным." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Число Фибоначчи F(" << n << ") = " << fibonacci(n) << std::endl;
+    return 0;
+}
